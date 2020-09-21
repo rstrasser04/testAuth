75123606e6555118cfb76b507a22830f3bd64a98
Ryan <shuttsie@gmail.com>
2020-09-21 11:42:08 -0500

    test

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -7,7 +7,7 @@ import { useAuth0, withAuthenticationRequired } from '@auth0/auth0-react';
 
 export const ProfileComponent = () => {
   const { user } = useAuth0();
-
+  console.log(user);
   return (
     <Container className='mb-5'>
       <Row className='align-items-center profile-header mb-5 text-center text-md-left'>
@@ -20,11 +20,30 @@ export const ProfileComponent = () => {
         </Col>
         <Col md>
           <h2>{user.name}</h2>
+
           <p className='lead text-muted'>{user.email}</p>
         </Col>
       </Row>
       <Row>
-        <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
+        <h2>
+          Name: {user[`https://fiservseminars.com/first_name`]}{' '}
+          {user[`https://fiservseminars.com/last_name`]}{' '}
+        </h2>
+      </Row>
+      <Row>
+        <h2>Bank Name: {user[`https://fiservseminars.com/bank_name`]}</h2>
+      </Row>
+      <Row>
+        <h2>Bank Title: {user[`https://fiservseminars.com/bank_title`]}</h2>
+      </Row>
+      <Row>
+        <h2>City: {user[`https://fiservseminars.com/city`]}</h2>
+      </Row>
+      <Row>
+        <h2>State: {user[`https://fiservseminars.com/state`]}</h2>
+      </Row>
+      <Row>
+        <h2>Seminar Date: {user[`https://fiservseminars.com/seminarDate`]}</h2>
       </Row>
     </Container>
   );
